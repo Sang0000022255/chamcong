91c55e473357665222a1dcc634a311edaddf0a38
Nguyễn Thái Sang <146358825+Sang0000022255@users.noreply.github.com>
2024-11-28 21:34:47 +0700

    Delete employees.js

--- employees.js
+++
@@ -1,5 +0,0 @@
-const employees = [
-  { name: "Nguyễn Văn A", code: "NV001", position: "Nhân viên" },
-  { name: "Trần Thị B", code: "NV002", position: "Quản lý" },
-  { name: "Lê Minh C", code: "NV003", position: "Giám đốc" },
-];
